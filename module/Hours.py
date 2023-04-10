d09eafcf9234bdeb8cc993e9ab03484ce45162b9
Kite-house <hodosok1301@gmail.com>
2023-04-10 08:34:04 +0700

    Version2.3.2

--- module/Hours.py
+++ module/Hours.py
@@ -3,6 +3,7 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 from module import hours_json, bet_json
+from Logging.logging import Logging
 
 async def hours(message):
     button1, button2, button3 = KeyboardButton('Добавить часов.'), KeyboardButton('Посмотреть кол-во часов.'), KeyboardButton('Удалить часы')
@@ -15,21 +16,26 @@ async def add_hours(message, value):
         hours_json.add_hours(username=message['from']['username'],value = value)
         bet = bet_json.read_bet(username=message['from']['username'])
         await message.reply(f'Записал! Количество отработаных часов: {value}, заработано денег: {value * bet}р.')
-        
+        Logging.action('ADD', 'HOURS', 'SUCCES')
+
     except IndexError or ValueError:
         await message.reply('Введите значение!')
+        Logging.action('ADD', 'HOURS', 'FAIL')
 
 
 async def read_hours(message):
     value = hours_json.read_hours(username=message['from']['username'])
     bet = bet_json.read_bet(username=message['from']['username'])
     await message.reply(f"Количество всего отработаных часов: {value}, всего заработано денег {value * bet}р.")
+    Logging.action('WATCH', 'HOURS', 'SUCCES')
 
 
 async def del_hours(message, value):
     try:
         hours_json.del_hours(username=message['from']['username'],value=value)
         await message.reply(f'Записал! Удалено часов: {value}.')
+        Logging.action('DELETE', 'HOURS', 'SUCCES')
 
     except IndexError or ValueError:
         await message.reply('Введите значение!')
+        Logging.action('DELETE', 'HOURS', 'FAIL')
