11641d1caa8b85e1dbf3eae4807384928c60263b
kite-house <88319088+kite-house@users.noreply.github.com>
2023-04-12 21:17:56 +0700

    Update db.py

--- DataBase/db.py
+++ DataBase/db.py
@@ -22,7 +22,7 @@ def conductor(username):
     database = os.listdir(path=dir_database)
     for file in database:
         if file == filename:
-            filename = f'{username}.json'
+            filename = f'{database}/{username}.json'
             return filename
 
     create_new_user_database(username=username)
@@ -35,7 +35,7 @@ def laod_full_database():
         file_find_json = file.split('.')
         try:
             if file_find_json[1] == 'json':
-                file = file.replace('.json','')
+                #file = file.replace('.json','')
                 full_database.append(file)
         except IndexError: pass
     return full_database
